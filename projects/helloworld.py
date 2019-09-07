8cad732fe2d4636c3edb5befcb9a3e5a478719cf
JungeunKim <redcranberry.kim@gmail.com>
2019-09-07 12:27:53 +0900

    My Django Girls app, first commit

---
+++ projects/helloworld.py
@@ -0,0 +1,14 @@
+def hi(name):
+	print('Hi' + name +'!')
+
+girls = ['jackie', 'monica', 'djodj']
+for name in girls:
+	hi(name)
+	print('Next girl')
+	#if name == 'Ola':
+	#	print('Hello Ola')
+	#elif name == 'jackie':
+	#	print('Hello jackie')
+	#else: 
+	#	print('djklf')
+
